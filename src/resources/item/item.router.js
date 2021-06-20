5d0fb60e8ea83927b4e54dc149ecc64f5aa0b1b6
thomatou <tc408@ic.ac.uk>
2021-06-20 16:07:31 -0700

    solution to exercise 2

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,5 +1,24 @@
 import { Router } from 'express'
 
+function controller(req, res) {
+  res.send({ message: 'hello' })
+}
+
 const router = Router()
+// equivalent to: /api/item
+// because we're mounting on there in the server.js file.
+router
+  .route('/')
+  .get(controller)
+  .post(controller)
+
+// Equivalent to '/api/item/id' because mounted on /api/item
+router
+  .route('/:id')
+  .get(controller)
+  .put(controller)
+  .delete(controller)
 
 export default router
+// Export default means that when you import this in another file, you can
+// just rename the object whatever you want
