023aae692f2b134d4a89244ff206b84f0cef1f5d
thomatou <tc408@ic.ac.uk>
2021-06-20 22:42:31 -0700

    solution to lesson 3

--- src/resources/item/item.model.js
+++ src/resources/item/item.model.js
@@ -1,4 +1,37 @@
 import mongoose from 'mongoose'
 
-const itemSchema = new mongoose.Schema({}, { timestamps: true })
+const itemSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      maxlength: 50,
+      required: true,
+      trim: true
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: ['active', 'complete', 'pastdue'],
+      default: 'active'
+    },
+    notes: String,
+    createdBy: {
+      ref: 'user',
+      required: true,
+      type: mongoose.SchemaTypes.ObjectId
+    },
+    due: Date,
+    list: {
+      ref: 'list',
+      required: true,
+      type: mongoose.SchemaTypes.ObjectId
+    }
+  },
+  { timestamps: true }
+)
+
+// This ensures that you can't create two items that belong to the same list
+// that have the same name. Called compound index.
+itemSchema.index({ list: 1, name: 1 }, { unique: true })
+
 export const Item = mongoose.model('item', itemSchema)
